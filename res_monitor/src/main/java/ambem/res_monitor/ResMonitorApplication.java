0dfb67109b7fe90747bed8927fadecd734cd980a
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2023-11-04 21:16:56 +0100

    Boilerplate

---
+++ res_monitor/src/main/java/ambem/res_monitor/ResMonitorApplication.java
@@ -0,0 +1,13 @@
+package ambem.res_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResMonitorApplication.class, args);
+	}
+
+}
---
+++ res_monitor/src/test/java/ambem/res_monitor/ResMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ambem.res_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
